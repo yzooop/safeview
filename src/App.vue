3aac0138ab5699d2ef06dacef4fb00b3deb65bc9
veronica <qkrduswn0404@gmail.com>
2024-12-09 15:29:36 +0900

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Modal from './components/ModalComponent.vue'
+
+let isModalOpen = false
+
+const toggleModal = () => {
+	isModalOpen = !isModalOpen
+}
+
+const closeModal = () => {
+	isModalOpen = false
+}
+</script>
+
+<template>
+	<button @click="toggleModal">열기</button>
+	<p>모달 : {{ isModalOpen }}</p>
+	<Modal v-if="isModalOpen" @close="closeModal"></Modal>
+</template>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+	props: {
+		isModalOpen: Boolean,
+	},
+	methods: {
+		close() {
+			this.$emit('close')
+		},
+	},
+}
+</script>
+
+<template>
+	<div class="Modal" v-if="isModalOpen">
+		안녕
+		<button @click="close">닫기</button>
+	</div>
+</template>
+
+<style>
+.Modal {
+	width: 100px;
+	height: 100px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid black;
+}
+</style>
